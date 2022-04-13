13da4bf11bee5836b63adabf5f35809bd107f903
yashpatil007 <yashpatil85326@gmail.com>
2022-04-13 10:34:23 +0530

    MODIFIED: Hyrachicle Inheritance to get Student Marks

--- src/com/infogalaxy/inherit/Student.java
+++ src/com/infogalaxy/inherit/Student.java
@@ -7,18 +7,53 @@ public class Student {
     int id;
     String Name;
 
-    void setStud() {
+    void getStud() {
+        System.out.println("-------------------------");
         System.out.println("Enter the ID No & Name : ");
         id = scanner.nextInt();
         Name = scanner.next();
     }
 }
 class Internal extends Student {
-    int im1 ;
-    int im2 ;
+    int im1;
+    int im2;
+
     void getInternal() {
         System.out.println("Internal Marks : ");
         im1 = scanner.nextInt();
         im2 = scanner.nextInt();
+    }
+    void setStud() {
+        System.out.println("Student Id : "+id);
+        System.out.println("Student Name : "+Name);
+        System.out.println("Internal Marks : "+im1);
+        System.out.println("Internal Marks : "+im2);
+    }
+}
+class External extends Student {
+    int em1 ;
+    int em2 ;
+    void getExternal() {
+        System.out.println("Enter the External Marks : ");
+        em1 = scanner.nextInt();
+        em2 = scanner.nextInt();
+    }
+    void setExternal () {
+        System.out.println("Student ID : "+id);
+        System.out.println("Student Name : "+Name);
+        System.out.println("External Marks : "+em1);
+        System.out.println("External Marks : "+em2);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Internal internal = new Internal();
+        External external = new External();
+        internal.getStud();
+        internal.getInternal();
+        internal.setStud();
+        external.getStud();
+        external.getExternal();
+        external.setExternal();
     }
 }
